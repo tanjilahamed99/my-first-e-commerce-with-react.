4dbc217ee49aee066ef7bfa66bfdf88bfe8ef504
Tanjil Ahamed <tanjil113355@gmail.com>
2023-09-14 12:50:26 +0600

    shirt area complete

---
+++ src/component/cart/cart.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+const cart = props => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+cart.propTypes = {
+    
+};
+
+export default cart;
--- src/component/shirt-con/shirts/shirts.jsx
+++ src/component/shirt-con/shirts/shirts.jsx
@@ -2,6 +2,7 @@
 
 import { useEffect } from "react";
 import { useState } from "react";
+import Shirt from "../shrit/shirt";
 
 const Shirts = () => {
     const [shirts, setShirts] = useState([]);
@@ -11,11 +12,13 @@ const Shirts = () => {
             .then(data => setShirts(data))
     }, [])
     return (
-        <div>
-            <h2 className='text-center font-bold text-3xl mt-10'>our best shirts</h2>
-            {
-                shirts.map((shirt) =>console.log(shirt))
-            }
+        <div className="w-[75%]">
+            <h2 className='text-center font-bold text-4xl mt-10 mb-3'>our best shirts</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 p-5">
+                {
+                    shirts.map((shirt, idx) => <Shirt key={idx} shirt={shirt}></Shirt>)
+                }
+            </div>
         </div>
     );
 };
